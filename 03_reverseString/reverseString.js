ab80cdab49ebeedacc802e87491816ba4868851b
Jade Bugnos <173978908+jadebugnos@users.noreply.github.com>
2024-09-12 14:12:06 +0800

    Fix to return the input string in reverse order

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stringArray = string.split('');
+    const reversed = [];
 
+    while (stringArray.length) {
+        reversed.push(stringArray.pop());
+    }
+
+    return reversed.join("");
 };
 
 // Do not edit below this line
