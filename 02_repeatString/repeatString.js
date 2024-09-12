36da1042c7932a42ca1cd6ccd77997cb2e7f1b57
Jade Bugnos <173978908+jadebugnos@users.noreply.github.com>
2024-09-12 10:44:33 +0800

    Ensure repeatString returns the expected output

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+let repeat = "";
 
+for (let i = 1; i <= num; i++) {
+    repeat += string;
+}
+
+return repeat;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
