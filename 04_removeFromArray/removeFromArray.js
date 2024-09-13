46005df364d90f0b65865fd055ab5ca57e38c1db
Jade Bugnos <173978908+jadebugnos@users.noreply.github.com>
2024-09-13 09:24:30 +0800

    -Filters an array by matching items from others
    -Accepts multiple arguments.
    -Filters the first array by removing items found
    in the rest.
    -Returns the filtered first array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...restArg) { 
+    let firstArgument = array;
+    let restArguments = restArg;
+   
+
+    return firstArgument.filter(item => !restArguments.includes(item));
 };
 
 // Do not edit below this line
