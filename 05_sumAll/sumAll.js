d581a02dff0e522048643db9303fa3699f560991
Jade Bugnos <173978908+jadebugnos@users.noreply.github.com>
2024-09-13 15:28:55 +0800

    -Add loop to sum numbers between the parameters

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
+const sumAll = function(...arg) {
+let sum = arg[0] - 1;
 
+for (let i = arg[0]; i <= arg[1]; i++) {
+    sum += i;
+}
+ return sum;
 };
 
 // Do not edit below this line
