d2971025203fe11e25f2c607c3abc03fb7d9fe2a
Jade Bugnos <173978908+jadebugnos@users.noreply.github.com>
2024-09-13 16:00:58 +0800

    fix function to return an Error if negative int

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,16 @@
-const sumAll = function(...arg) {
-let sum = arg[0] - 1;
+const sumAll = function (...arg) {
+    let sum = 0;
 
-for (let i = arg[0]; i <= arg[1]; i++) {
-    sum += i;
-}
- return sum;
+    if (arg[0] > arg[1]) {
+        arg.reverse();
+    } else if (arg[0] < 0 || arg[1] < 0) {
+        return "ERROR";
+    }
+
+    for (let i = arg[0]; i <= arg[1]; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
