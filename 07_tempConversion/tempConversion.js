f26e43f9728642efa904951caff5e0418130c7b3
Jade Bugnos <173978908+jadebugnos@users.noreply.github.com>
2024-09-14 09:05:42 +0800

    fix to convert celcius to fahrenhiet & vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const toBeRounded = (temp - 32) / 1.8;
+  const rounded = Math.round(toBeRounded * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const toBeRounded = (temp * 1.8) + 32;
+  const rounded = Math.round(toBeRounded * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
