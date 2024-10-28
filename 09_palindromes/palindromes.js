2fe62b2a8f0987db0ea004aea0003786e338a011
Jade Bugnos <173978908+jadebugnos@users.noreply.github.com>
2024-10-28 11:46:44 +0800

    edit function to pass all test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (...string) {
+    let filtered = string
+        .map(item => item.toLowerCase()) // for case insensitivity
+        .filter(item => typeof item === 'string') // omit all non alphabet
+        .join('') //to convert the array back to string
 
+    filtered = filtered.replace(/[^a-z0-9]/g, ''); // filters all non-string items
+
+    let reversed = filtered
+        .split('') //convert to array
+        .reverse() //reverse the order of items
+        .join(''); // convert back to string
+    return reversed === filtered ? true : false;
 };
-
+//if (index === lastIndexof(item))
 // Do not edit below this line
 module.exports = palindromes;
