23efa7a28d9ad149e88c5c1d40df5ee23288832c
Jade Bugnos <173978908+jadebugnos@users.noreply.github.com>
2024-10-28 09:15:07 +0800

    edit remaining function to pass all test

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function(a, b) {
-	return a + b;
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function(a, b) {
-	return a - b;
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (sum) {
+  return sum.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (times) {
+  return times.reduce((a, b) => a * b, 1);
 };
 
-const power = function() {
-	
+const power = function (...numbers) {
+  return numbers.reduce((a, b) => a ** b);
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
