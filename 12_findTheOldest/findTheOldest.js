67b93f23da9eddf50283c7060d23237e30d6ef3a
Jade Bugnos <173978908+jadebugnos@users.noreply.github.com>
2024-10-30 09:31:30 +0800

    edit function to return oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    let transform = people
+        .map(person => {
+            const currentYear = new Date().getFullYear()
+            const yearOfDeath = (person.yearOfDeath || currentYear)
+            const age = yearOfDeath - person.yearOfBirth;
+            return {
+                ...person,
+                yearOfDeath,
+                age
+            }
+        })
+        .sort((a, b) => a.age - b.age)
 
+    return transform.pop()
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
